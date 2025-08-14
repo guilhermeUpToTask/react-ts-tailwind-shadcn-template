5bbca41fa1a8b324c2d4500193f6547e89c974f2
guilhermeUpToTask <guilherme256teretelteltel@gmail.com>
2025-08-14 09:45:11 -0300

    chore(client): implemented a methodName Builder to reduce redudancie in the functions of the classes in the sdk

--- openapi-ts.config.ts
+++ openapi-ts.config.ts
@@ -1,35 +1,40 @@
-import { defineConfig } from "@hey-api/openapi-ts"
+import { defineConfig } from "@hey-api/openapi-ts";
 //This is a updated config for openapi-ts, we need to install biome later
 //We can test later to use the tanstack query plugin
 export default defineConfig({
-  //we can set this later to point to the backend source file
-  input: "./openapi.json",
-  output: {
-    lint: 'eslint',
-    // lint: 'biome'
-    //format: 'biome'
-    path: 'src/client',
-  },
-  plugins: ['@hey-api/client-axios']
-  //needs to read the docs to achive the same pattern on the axios client sdk, to achive a cleaner method names in the generated SDK
-  /*
-      {
-      name: "@hey-api/sdk",
-      // NOTE: this doesn't allow tree-shaking
-      asClass: true,
-      operationId: true,
-      methodNameBuilder: (operation) => {
-        // @ts-ignore
-        let name: string = operation.name
-        // @ts-ignore
-        let service: string = operation.service
-
-        if (service && name.toLowerCase().startsWith(service.toLowerCase())) {
-          name = name.slice(service.length)
-        }
-
-        return name.charAt(0).toLowerCase() + name.slice(1)
-      },
+    //we can set this later to point to the backend source file
+    input: "./openapi.json",
+    output: {
+        lint: "eslint",
+        // lint: 'biome'
+        //format: 'biome'
+        path: "src/client",
     },
-  */
-})
+
+    plugins: [
+        {
+            name: "@hey-api/client-axios",
+        },
+        {
+            name: "@hey-api/sdk",
+            asClass: true,
+            operationId: true,
+            // Generates SDK method names from OpenAPI operations. Uses the operation's id as the base name, defaults to 'unnamedOperation' if missing, strips the first tag (service name) from the start to avoid redundancy, and converts the first character to lowercase for camelCase style.
+            methodNameBuilder: (operation) => {
+                let name: string = operation.id ?? "unnamedOperation";
+
+                const service =
+                    "tags" in operation ? operation.tags?.[0] ?? "" : "";
+
+                if (
+                    service &&
+                    name.toLowerCase().startsWith(service.toLowerCase())
+                ) {
+                    name = name.slice(service.length);
+                }
+
+                return name.charAt(0).toLowerCase() + name.slice(1);
+            },
+        },
+    ],
+});
